292cfa9c06809b83a8155b5fc169d54bcb892111
jq1836 <jordanquahshaoxuan@gmail.com>
2025-03-19 15:10:06 +0800

    Cleanup sarw.py

--- sarw.py
+++ sarw.py
@@ -1,44 +1,34 @@
 import numpy as np
+import random
 
-# Returns the new coordinate after taking action
-def step(action, coord):
-    match action:
-        case 0: # up
-            return coord[0], coord[1]-1
-        case 1: # down
-            return coord[0], coord[1]+1
-        case 2: # left
-            return coord[0]-1, coord[1]
-        case 3: # right
-            return coord[0]+1, coord[1]
-    return None # invalid action taken
+# Returns a list of possible next coordinates
+def get_candidates(coord, visited):
+    candidates = []
+    up = (coord[0], coord[1]-1)
+    down = (coord[0], coord[1]+1)
+    left = (coord[0]-1, coord[1])
+    right = (coord[0]+1, coord[1])
+    if up not in visited: candidates.append(up)
+    if down not in visited: candidates.append(down)
+    if left not in visited: candidates.append(left)
+    if right not in visited: candidates.append(right)
+    return candidates
 
-# Checks if there are any possible steps
-def isStuck(coord, visited):
-    return (coord[0], coord[1]-1) in visited\
-        and (coord[0], coord[1]+1) in visited\
-        and (coord[0]-1, coord[1]) in visited\
-        and (coord[0]+1, coord[1]) in visited
-
-def generateSARW(n):
-    actions = []
+def generate_SARW(n):
     coords = [(0,0)]
-    visited = set([(0,0)])
-    while len(actions) < n:
-        action = np.random.randint(0, 4)
-        if isStuck(coords[-1], visited): # Restarts the process
-            actions = []
+    visited = set(coords)
+    while len(coords) <= n:
+        candidates = get_candidates(coords[-1], visited)
+        if len(candidates) == 0:
             coords = [(0,0)]
-            visited = set([(0,0)])
-        newCoord = step(action, coords[-1])
-        if newCoord not in visited:
-            actions.append(action)
-            coords.append(newCoord)
-            visited.add(newCoord)
-    return coords, actions
+            visited = set(coords)
+        coords.append(random.choice(candidates))
+    return coords
 
 if __name__ == '__main__':
-    for i in range(10_000):
-        generateSARW(50)
-
+    # test speed of implementation
+    for i in range(100_000):
+        generate_SARW(50)
+    # smoke test for correctness
+    print(generate_SARW(10))
 
